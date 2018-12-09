a9c64fed0bc1e440f51e20d88c5c4cc0d64c7c5a
DoleTM <adylantoremember@gmail.com>
2018-12-09 15:33:26 -0500

    Initial framework commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+// Declare the 'global variables to be used in this game.
+var button1 = placeholderValue;
+var button2 = placeholderValue;
+var button3 = placeholderValue;
+var button4 = placeholderValue;
+
+// Write the functions to be used for this game...
+// This function does.....
+function placeholder(){
+
+};
+// This function does.....
+function placeholder(){
+    
+};
+// This function does.....
+function placeholder(){
+    
+};
+// This function does.....
+function placeholder(){
+    
+};
+// This function does.....
+function placeholder(){
+    
+};
+// Start the game once the page has loaded
+$(document).ready(function(){
+
+
+
+
+
+
+
+
+
+
+    
+});
